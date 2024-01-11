d1c1594bb66d2bdbad9f707cb8ec2ed835bbd953
SebastianTMS <sebastian.villada@tmsolutions.com.co>
2024-01-10 21:29:30 -0500

    Creación de proyecto de Cronómetro

---
+++ view/js/script.js
@@ -0,0 +1,69 @@
+// Convierte la hora a un formato de horas, minutos, segundos y milisegundos
+function timeToString(time) {
+  let diffInHrs = time / 3600000;
+  let hh = Math.floor(diffInHrs);
+
+  let diffInMin = (diffInHrs - hh) * 60;
+  let mm = Math.floor(diffInMin);
+
+  let diffInSec = (diffInMin - mm) * 60;
+  let ss = Math.floor(diffInSec);
+
+  let diffInMs = (diffInSec - ss) * 100;
+  let ms = Math.floor(diffInMs);
+
+  let formattedMM = mm.toString().padStart(2, "0");
+  let formattedSS = ss.toString().padStart(2, "0");
+  let formattedMS = ms.toString().padStart(2, "0");
+
+  return `${formattedMM}:${formattedSS}:${formattedMS}`;
+}
+
+// Declarar variables para usar en nuestras funciones a continuación
+let startTime;
+let elapsedTime = 0;
+let timerInterval;
+
+// Crea una función para modificar el HTML interno
+function print(txt) {
+  document.getElementById("display").innerHTML = txt;
+}
+
+// Crear funciones de "inicio", "pausa" y "reinicio"
+function start() {
+  startTime = Date.now() - elapsedTime;
+  timerInterval = setInterval(function printTime() {
+    elapsedTime = Date.now() - startTime;
+    print(timeToString(elapsedTime));
+  }, 10);
+  showButton("PAUSE");
+}
+
+function pause() {
+  clearInterval(timerInterval);
+  showButton("PLAY");
+}
+
+function reset() {
+  clearInterval(timerInterval);
+  print("00:00:00");
+  elapsedTime = 0;
+  showButton("PLAY");
+}
+
+//Crear función para mostrar botones
+function showButton(buttonKey) {
+  const buttonToShow = buttonKey === "PLAY" ? playButton : pauseButton;
+  const buttonToHide = buttonKey === "PLAY" ? pauseButton : playButton;
+  buttonToShow.style.display = "block";
+  buttonToHide.style.display = "none";
+}
+
+// Crear detectores de eventos
+let playButton = document.getElementById("playButton");
+let pauseButton = document.getElementById("pauseButton");
+let resetButton = document.getElementById("resetButton");
+
+playButton.addEventListener("click", start);
+pauseButton.addEventListener("click", pause);
+resetButton.addEventListener("click", reset);
